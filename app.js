4edd7d4d7ff1fc56b837966b806c893fa557587f
zgulen <gulenzubeyir@gmail.com>
2022-06-17 18:25:37 +0300

    time and days created

---
+++ app.js
@@ -0,0 +1,56 @@
+//! day-week-year-monh variables
+const weekDay = new Date().getDay()
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+let dayName = days[weekDay];
+
+//! hour variables
+const hourDom = document.querySelector(".container__time--clock-hour")
+const minuteDom = document.querySelector(".container__time--clock-minute")
+const secondDom = document.querySelector(".container__time--clock-second")
+const milisecDom = document.querySelector(".container__time--clock-ms")
+//! year variables
+const yearDom = document.querySelector(".conteiner__date--days-year")
+const monthDom = document.querySelector(".conteiner__date--days-month")
+const dayDom = document.querySelector(".conteiner__date--days-day")
+const weekDom = document.querySelector(".conteiner__date--days-week-day")
+
+setInterval(
+    function () {
+        console.log(new Date().getHours())
+        hourDom.textContent = new Date().getHours()
+        setInterval(
+            function () {
+                minuteDom.textContent = ` :${new Date().getMinutes()}`
+                setInterval(
+                    function () {
+                        secondDom.textContent = ` :${new Date().getSeconds() <= 9 ? "0" + new Date().getSeconds() : new Date().getSeconds()}`
+                        setInterval(
+                            function () {
+                                milisecDom.textContent = `:${(String(new Date().getMilliseconds()).padStart(2, "0") / 10).toFixed(0) <= 9 ? "0" + (String(new Date().getMilliseconds()).padStart(2, "0") / 10).toFixed(0) : (String(new Date().getMilliseconds()).padStart(2, "0") / 10).toFixed(0)}`
+                            })
+                    })
+            })
+    })
+
+setInterval(
+    function () {
+        yearDom.textContent = `${new Date().getFullYear()}-`
+        setInterval(() => {
+            monthDom.textContent = `${new Date().getMonth() + 1}-`
+            setInterval(() => {
+                dayDom.textContent = `${new Date().getDate()}`
+                setInterval(() => {
+                    weekDom.textContent = `  :${dayName}`
+                });
+            });
+        });
+    }
+)
+
+
+
+
+// const seconds = String(new Date().getSeconds()).padStart(2, '0');
+// console.log(seconds)
+
+// console.log((String(new Date().getMilliseconds()).padStart(3,"0")/ 10).toFixed(0))
